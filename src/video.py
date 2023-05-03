ba8f7cbbc45d1cfd8112011afccab7a1beb7f980
Konstantin Yakovlev <yakovlev.konstantin.s@gmail.com>
2023-05-03 21:12:12 +0900

    complete homework 6

--- src/video.py
+++ src/video.py
@@ -1,5 +1,8 @@
 import os
+
+import requests.exceptions
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class Video:
@@ -10,12 +13,18 @@ class Video:
     def __init__(self, video_id: str) -> None:
         """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
         self.__video_id = video_id
-        video = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id)\
-            .execute()
-        self.title: str = video['items'][0]['snippet']['title']
-        self.url = f'https://www.youtube.com/channel/{self.__video_id}'
-        self.view_count = video['items'][0]['statistics']['viewCount']
-        self.like_count = video['items'][0]['statistics']['likeCount']
+        try:
+            video = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id)\
+                .execute()
+            self.title: str = video['items'][0]['snippet']['title']
+            self.url = f'https://www.youtube.com/channel/{self.__video_id}'
+            self.view_count = video['items'][0]['statistics']['viewCount']
+            self.like_count = video['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
 
     def __str__(self):
